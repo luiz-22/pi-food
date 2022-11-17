a15994f6acd879d600871fdab18c3e8426694d9b
Luis <luismanlaz@hotmail..com>
2022-11-17 00:01:47 -0300

    16/11/2022

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import styles from "./Footer.module.css";
-import { ReactComponent as github } from '../../assets/github.svg';
+import github from "../../assets/github.svg";
+import linkedin from "../../assets/linkedin.svg";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <section className="container">
-        <img src={github} alt="" />
+        <img className={styles.brand} src={linkedin} alt="linkedin" />
+        <img className={styles.brand} src={github} alt="github" />
       </section>
     </div>
   );
