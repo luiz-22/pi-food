434c347ac7abd99c604ee4eab84cefbb076b8630
Luis <luismanlaz@hotmail..com>
2023-01-22 20:39:49 -0300

    22/01/2023

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -11,13 +10,11 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', 'https://recipe-inn.netlify.app');
   res.header('Access-Control-Allow-Origin', 'https://recipe-inn.vercel.app');
